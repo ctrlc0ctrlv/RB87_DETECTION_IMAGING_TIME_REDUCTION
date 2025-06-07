ff3f70c47aaf1738e834764a0a714935bc0a075e
ctrlc0ctrlv <vrbyov12@gmail.com>
2025-06-07 20:41:43 +0300

    Updated setup_conda_env_with_jupyter_kernel.sh

--- setup_conda_env_with_jupyter_kernel.sh
+++ setup_conda_env_with_jupyter_kernel.sh
@@ -1,3 +1,5 @@
 conda create --name rb87_detection_env --file ./requirements.txt -c conda-forge -y
+conda init --all
+source activate rb87_detection_env
 conda activate rb87_detection_env
 python -m ipykernel install --user --name rb87_detection_env
