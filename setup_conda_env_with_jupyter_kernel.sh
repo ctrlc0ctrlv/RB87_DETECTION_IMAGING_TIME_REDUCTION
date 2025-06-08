5793c3131959059e4445df12027efd62f29992ed
ctrlc0ctrlv <vrbyov12@gmail.com>
2025-06-08 23:22:14 +0300

    Updated conda env setup script

--- setup_conda_env_with_jupyter_kernel.sh
+++ setup_conda_env_with_jupyter_kernel.sh
@@ -1,5 +1,6 @@
 conda create --name rb87_detection_env --file ./requirements.txt -c conda-forge -y
 conda init --all
+exec $SHELL
 source activate rb87_detection_env
 conda activate rb87_detection_env
 python -m ipykernel install --user --name rb87_detection_env
